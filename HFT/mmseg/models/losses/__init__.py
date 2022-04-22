2d216636e83dc2457b971ea383b8b2840c32f39e
JiayuZou2020 <77472466+JiayuZou2020@users.noreply.github.com>
2022-04-22 18:19:44 +0800

    training losses

---
+++ HFT/mmseg/models/losses/__init__.py
@@ -0,0 +1,12 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from .accuracy import Accuracy, accuracy
+from .cross_entropy_loss import (CrossEntropyLoss, binary_cross_entropy,
+                                cross_entropy, mask_cross_entropy)
+from .utils import reduce_loss, weight_reduce_loss, weighted_loss
+from .iou import iou
+from .occupancy import occupancyloss
+__all__ = [
+    'accuracy', 'Accuracy', 'cross_entropy', 'binary_cross_entropy',
+    'mask_cross_entropy', 'CrossEntropyLoss', 'reduce_loss',
+    'weight_reduce_loss', 'weighted_loss','iou','occupancyloss'
+]
