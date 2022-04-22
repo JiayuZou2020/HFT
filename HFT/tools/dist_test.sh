808bbdeb7501af18b2676f463c8e6ce3106ac384
JiayuZou2020 <77472466+JiayuZou2020@users.noreply.github.com>
2022-04-22 17:26:38 +0800

    a script to test model

--- HFT/tools/dist_test.sh
+++ HFT/tools/dist_test.sh
@@ -5,5 +5,5 @@ CHECKPOINT=$2
 GPUS=$3
 PORT=${PORT:-29500}
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
-/mnt/cfs/algorithm/jiayu.zou/anaconda3/bin/python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
+python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
     $(dirname "$0")/test.py $CONFIG $CHECKPOINT --launcher pytorch ${@:4}
